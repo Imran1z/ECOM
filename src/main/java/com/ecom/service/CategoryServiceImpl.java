1038c184958fa729513451ab56c456a802289b80
imran1z <shaikhzimran12334@gmail.com>
2025-10-31 08:42:05 +0530

    Moved the code from plain list to Jpa repository

--- src/main/java/com/ecom/service/CategoryServiceImpl.java
+++ src/main/java/com/ecom/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecom.service;
 
 import com.ecom.Model.Category;
+import com.ecom.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,21 +13,24 @@ import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
-    private List<Category>categories =new ArrayList<>();
+    @Autowired
+    private CategoryRepository categoryRepository;
+//    /private List<Category>categories =new ArrayList<>();
     private Long nextId=1L;
     @Override
     public List<Category> getAllCategory() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
+        List<Category> categories=categoryRepository.findAll();
         Category category =categories.stream()
                 .filter(c->c.getCategoryId().equals(categoryId))
                 .findFirst()
@@ -34,12 +39,13 @@ public class CategoryServiceImpl implements CategoryService{
 //        if(category==null){
 //            return "No category with ID: "+categoryId+" found";
 //        }
-        categories.remove(category);
+        categoryRepository.delete(category);
         return "Category with categoryId: "+categoryId+" deleted successfully";
     }
 
     @Override
     public Category updateCategory(Category category,Long categoryID) {
+        List<Category> categories=categoryRepository.findAll();
         Optional<Category>category1=categories.stream().filter(c->c.getCategoryId().equals(categoryID))
                 .findFirst();
 
@@ -47,6 +53,7 @@ public class CategoryServiceImpl implements CategoryService{
             Category existingCategory=category1.get();
             existingCategory.setCategoryName(category.getCategoryName());
             existingCategory.setCategoryId(category.getCategoryId());
+            categoryRepository.save(existingCategory);
             return existingCategory;
         }else{
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found");
