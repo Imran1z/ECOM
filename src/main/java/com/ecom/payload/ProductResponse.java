d7b157e26ed5aec96a0d59219f643548f3dce635
imran1z <shaikhzimran12334@gmail.com>
2025-11-18 21:49:52 +0530

    Setting up the DTOs

---
+++ src/main/java/com/ecom/payload/ProductResponse.java
@@ -0,0 +1,15 @@
+package com.ecom.payload;
+
+import com.ecom.Model.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponse {
+    private List<Product> content;
+}
